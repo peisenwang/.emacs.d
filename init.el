c056d258a5475e8642251e168f83eff2abcf5531
Peisen Wang <wpeisen@gmail.com>
2020-10-13 00:46:21 +0800

    Merge from old config.

--- init.el
+++ init.el
@@ -11,10 +11,13 @@
 ;;   ===============
 (setq inhibit-startup-message t
       initial-scratch-message nil)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
 ;;(fringe-mode 0)
+
+;; Set title
+(setq frame-title-format '("%f"))
 
 ;; Expand to full height
 (setq default-frame-alist '((fullscreen . fullheight)))
@@ -76,20 +79,23 @@
 (setq tramp-auto-save-directory auto-save-file-directory)
 
 ;; Save history for M-x etc.
-(savehist-mode 1)
+(savehist-mode t)
 (setq savehist-additional-variables '(kill-ring search-ring regexp-search-ring))
 
 
 ;;;; General settings
 ;;   ================
+;; Auto load changed files
+(global-auto-revert-mode t)
+
+;; recentf
+(recentf-mode t)
+
 ;; No type full yes
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; Fast show prefix keys in echo area
 (setq echo-keystrokes 0.1)
-
-;; recentf
-(recentf-mode t)
 
 ;; Scrolling
 (setq scroll-margin 3
@@ -98,10 +104,12 @@
       scroll-down-aggressively 0.01
       scroll-preserve-screen-position t
       auto-window-vscroll nil
+      ;; C-v and M-v scroll to the end
       scroll-error-top-bottom t)
 
 ;; environment path
 (setenv "PATH" (concat "~/.local/bin" (getenv "PATH")))
+
 
 ;;;; Editing
 ;;   =======
@@ -111,30 +119,51 @@
 
 ;; Show parentheses
 (show-paren-mode t)
+(setq show-paren-style 'mixed)
 
 ;; auto parenthese pair
 (add-hook 'prog-mode-hook #'electric-pair-mode)
 
+;; Larger kill ring size
+(setq kill-ring-max 500)
+
+;; EOF newline
+(setq require-final-newline t)
+
 ;; Show column number
 (setq column-number-mode t)
+
+;; Do not show line number at bottom
+(setq line-number-mode nil)
+
+;; Better display for files with the same name
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
 
 
 ;;;; Key-bindings
 ;;   ============
 ;; Use "C-x C-o" instead of "C-x o" to switch windows
+;; (originally `delete-blank-lines')
 (global-set-key (kbd "C-x C-o") 'other-window)
 
 ;; Switch "C-x b" and "C-x C-b" for buffer switching
 (global-set-key (kbd "C-x b") 'list-buffers)
 (global-set-key (kbd "C-x C-b") 'switch-to-buffer)
 
-;; Kill current buffer with "C-x C-k"
+;; Kill current buffer with "C-x C-k" (originally `kill-buffer')
 (global-set-key (kbd "C-x C-k") 'kill-this-buffer)
 
 ;; Create nextline for "C-o" (originally `open-line')
 (global-set-key (kbd "C-o") (kbd "C-e C-M"))
 ;; Create a previous line for "C-j" (originally `eval-print-last-sexp')
 (global-set-key (kbd "C-j") (kbd "C-a C-M C-p"))
+
+;; Enable type return when holding control
+(global-set-key (kbd "<C-return>") (kbd "RET"))
+
+;; Comment region
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
 
 ;; Use "C-q" to switch window like what I do for tmux (originally `quoted-insert')
 (global-unset-key (kbd "C-q"))
@@ -150,9 +179,21 @@
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 
+;; Use command as meta and option as super for mac
+(setq mac-option-modifier 'super
+      mac-command-modifier 'meta)
+
 
 ;;;; Specific packages setting
 ;;   =========================
+;; ido
+(use-package ido
+  :config
+  (setq ido-everywhere t
+	ido-enable-flex-matching t
+	ido-use-filename-at-point 'guess)
+  (ido-mode t))
+
 ;; Auto delete trailing whitespaces.
 (use-package ws-butler
   :hook (prog-mode . ws-butler-mode))
@@ -160,7 +201,9 @@
 ;; Set line numbers with certain modes as exceptions
 (use-package linum-off
   :init
-  (global-linum-mode t))
+  (global-linum-mode t)
+  :config
+  (add-to-list 'linum-disabled-modes-list 'ansi-term))
 
 ;; elpy for python
 (use-package elpy
